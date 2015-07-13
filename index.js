50e6ef206944f98cff412baf99a18ccf5f7f6f12
Timothy J Laurent <timothyjlaurent@gmail.com>
2015-07-13 15:34:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ var request = require('superagent');
 // patch superagent to attach CSRF-token to all requests
 try {
   var csrf = document.cookie.match(/csrftoken=(.*?)(?:$|;)/)[1];
+  var end = request.Request.prototype.end;
+  request.Request.prototype.end = function(fn) {
+    this.set('X-CSRFToken', csrf);
+    return end.call(this, fn);
+  };
 }
 catch (err) {
-  return;
+  
 }
-var end = request.Request.prototype.end;
-request.Request.prototype.end = function(fn) {
-  this.set('X-CSRFToken', csrf);
-  return end.call(this, fn);
-};
+
